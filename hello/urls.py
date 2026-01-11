c054f41ebfa33f0922c36813bcd15252cda99351
jiaenkoh0327-ctrl <jiaenkoh0327@gmail.com>
2026-01-11 20:58:54 +0800

    OH SO [add README.md] only creates a read me lol

---
+++ hello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("", views.home, name="home"),
+    path("hello/<name>", views.hello_there, name="hello_there"),
+    path("index/",views.index, name="index")
+]
---
+++ hello/views.py
@@ -0,0 +1,35 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+# Replace the existing home function with the one below
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def index(request):
+    return render(request, "hello/index.html")
+
+
+
+
